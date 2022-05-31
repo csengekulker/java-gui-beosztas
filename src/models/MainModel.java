6516d3e5f50c262f607d6a5ac01265823666bf9d
csengekulker <k71608883429@ktch.hu>
2022-05-31 08:24:12 +0200

    Positions stored in array of objects

--- src/models/MainModel.java
+++ src/models/MainModel.java
@@ -1,5 +1,19 @@
 package models;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 public class MainModel {
-    
+    Position[] positionArray = {
+        new Position(1, "Igazgató"),
+        new Position(2, "Projektvezető"),
+        new Position(3, "Könyvelő"),
+        new Position(4, "HR Manager"),
+        new Position(5, "Informatikai vezető"),
+        new Position(6, "Logisztikai asszisztens"),
+        new Position(7, "Irodavezető"),
+        new Position(8, "Szoftvermérnök"),
+        new Position(9, "Szoftverfejlesztő"),
+        new Position(10, "Takarító"),
+    };
 }
---
+++ src/models/Position.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Position {
+    public int id;
+    public String name;
+
+    public Position(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
